8eca6f4d8ad74647fc874ac8575d1bcac1d43d70
JustTyping1 <aditya.chorghade@outlook.com>
2023-05-04 19:30:04 +0100

    Added file and rendering panel

---
+++ rendering.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class rendering {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        Container pane = frame.getContentPane();
+        pane.setLayout(new BorderLayout());
+
+        JPanel renderpanel = new JPanel() {
+
+        }
+    }
+}
